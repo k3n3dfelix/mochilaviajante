0dfc3e95765111610fd88d9d433763aaa0038eec
kenedfelix <kened.felix@gmail.com>
2021-12-10 23:12:29 -0300

    CheckpointIII Finalizado

--- programacao-imperativa/checkpoint-III/checkpointIII.js
+++ programacao-imperativa/checkpoint-III/checkpointIII.js
@@ -9,47 +9,25 @@ function Aluno(nome,qtd_faltas,notas){
     this.nome = nome
     this.qtd_faltas = qtd_faltas
     this.notas = notas
-    }
-   
+}
 
-    const alunos = [
-        new Aluno('Arthur Campolina', 1, [10,8,6,7]),
-        new Aluno('Cristian Fernandes', 0, [10,8,6,7]),
-        new Aluno('Felipe Santos ', 0, [10,8,6,7]),
-        new Aluno('Kened Silva', 0, [10,8,6,7]),
-        new Aluno('Rodrigo Costa Valle', 0, [10,8,6,7]),
-        new Aluno('Thiago', 0, [10,8,6,7]),
-    ];
-    console.log(alunos);
+const alunos = [
+    new Aluno('Arthur Campolina', 9, [10,8,6,7]),
+    new Aluno('Cristian Fernandes', 0, [10,8,6,7]),
+    new Aluno('Felipe Santos ', 0, [10,8,6,7]),
+    new Aluno('Kened Silva', 0, [10,8,6,7]),
+    new Aluno('Rodrigo Costa Valle', 0, [10,8,6,7]),
+    new Aluno('Thiago', 10, [10,8,6,7]),
+];
+
+
+console.log("1 - Objetos Alunos Criados__________________________________________________________________________")
+console.log(alunos);
 /* Micro desafio - Passo 2
 Nosso objeto aluno terá o método calcularMedia que retorna a média de suas notas. Também terá um método chamado faltas, que simplesmente aumenta o número de faltas em 1. */
 
 function CalcularMedia(Alunos){
-    //  let arrayAlunos = Alunos.map(aluno =>`${aluno.nome}`, `${aluno.notas}`);
-    //let arrayAlunos = Alunos.map(aluno =>`${aluno.nome}`);
-    //console.log(arrayNotas);
     
-    // let arrayMedia = [];
-    // let arrayNotas = Alunos.map(function(aluno){
-    //     let novoArray = [aluno.nome,aluno.notas]
-       
-    //      return novoArray;  
-    // })
-    
-    //  for(i=0;i<novoArray.length;i++){
-    //      for(j=0;j<=1;j++){
-
-    //      }
-    //         let total = novoArray[i].reduce(function(acumulador,item){
-    //             return acumulador+item;
-    //         });
-
-    //         arrayMedia.push(total)
-    //     }
-   
-//     let array = Alunos.filter((item)=>{item.notas});
-//    console.log();
-//     return array;
         let alunos = Alunos.map(function(aluno){
                 
             return aluno.nome;
@@ -76,7 +54,8 @@ function CalcularMedia(Alunos){
                 }
     return arrayMedia;
 }
-//console.log(CalcularMedia(alunos));
+console.log("2 - Media dos Alunos________________________________________________________________________________")
+console.log(CalcularMedia(alunos));
 
 function faltas(alunos){
     let ArrayAlunos = alunos.map(function(aluno){
@@ -93,19 +72,23 @@ function faltas(alunos){
 Em um arquivo diferente, crie o objeto literal curso que tem como atributos: nome do curso (string), nota de aprovação (number), faltas máximas (number) e uma lista de estudantes (um array composto pelos alunos criados no passo 1).
 */
 const Cursos = require('./modulos/curso');
-// console.log(Cursos());
+
+console.log("3 - Objeto Literal Cursos Criado____________________________________________________________________")
+console.log(Cursos());
 
 /* Micro desafio - Passo 4
 Crie o método que permite adicionar alunos à lista do curso, ou seja, quando chamamos nosso método em nosso objeto curso, deverá adicionar um aluno a mais na propriedade lista de estudantes do objeto curso.
 */
-//let curso1 = new Curso("Teste",7, 3,['fulano','ciclano']);
-//console.log(curso1);
+
+
 let alunoNovo = new Aluno('Isabela Wagner', 0, [10,8,6,7]);
 function adicionaAlunos(aluno){
-    alunos.push(aluno);
+alunos.push(aluno);
 }
 adicionaAlunos(alunoNovo);
-//console.log(alunos)
+
+console.log("4 - Adicionado um novo Aluno _______________________________________________________________________")
+console.log(alunos)
 
 /* Micro desafio - Passo 5
 Crie um método para o objeto curso que receba um aluno (como parâmetro) e retorne true se ele aprovou no curso ou false em caso de reprovação. Para ser aprovado, o aluno tem que ter uma média igual ou acima da nota de aprovação  e ter menos faltas que faltas máximas. Se tiver a mesma quantidade, tem que estar 10% acima da nota de aprovação.
@@ -130,7 +113,7 @@ function verificaAprovacao(aluno){
    }
   //console.log(cursos[0].faltas_maximas);
   if(array_comp[2] == cursos[0].faltas_maximas){
-    if(array_comp[1] > cursos[0].nota_aprovacao + (cursos[0].nota_aprovacao* 0.1)){
+    if(array_comp[1] > cursos[0].nota_aprovacao + (cursos[0].nota_aprovacao*0.1)){
         return true;
        }else{
            return false;
@@ -144,15 +127,61 @@ function verificaAprovacao(aluno){
   }
   
 }
-console.log(verificaAprovacao("Arthur Campolina"));
+console.log("5 - Verificando se um aluno foi aprovado ou não_______________________________________________________")
+console.log(verificaAprovacao("Thiago"));
 
 /* Micro desafio - Passo 6
 Crie um método para o objeto curso que percorra a lista de estudantes e retorne um array de booleanos com os resultados se os alunos aprovaram ou não.
 */
 
+function verificaAprovacaoGeral(){
+
+    let mediaAlunos = CalcularMedia(alunos);
+    let cursos = Cursos();
+    let array_comp = [];
+    let array_faltas = [];
+   
+   for(i=0;i<mediaAlunos.length;i++){
+
+      for(j=1;j<2;j++){
+            array_comp.push(mediaAlunos[i][j]);
+            array_faltas.push(mediaAlunos[i][j+1]);
+       }
+   }
+   array_boolean_aprovacao = [];
+   
+   for(i=0;i<array_comp.length;i++){
+       
+        if(array_faltas[i] == cursos[0].faltas_maximas){
+            if(array_comp[i] > cursos[0].nota_aprovacao + (cursos[0].nota_aprovacao* 0.1)){
+
+                array_boolean_aprovacao.push(true);
+              
+            }else{
+                array_boolean_aprovacao.push(false); 
+            }
+        }else{
+            if(array_comp[i] > cursos[0].nota_aprovacao && array_faltas[i] < cursos[0].faltas_maximas){
+                array_boolean_aprovacao.push(true);
+               
+               
+               
+            }else{
+                array_boolean_aprovacao.push(false);  
+            }
+        }
+  }
+  return array_boolean_aprovacao;
+ 
+}
+console.log("6 - Retornando Resultado de aprovação de todos os alunos______________________________________________")
+console.log(verificaAprovacaoGeral());
+
 /*Micro desafio - Passo 7
 Importe o módulo estudantes.js, que contém uma lista de alunos, dentro do arquivo que contém o objeto curso. Substitua o conteúdo da propriedade lista de estudantes, pela lista de estudantes do arquivo estudantes.js e garanta que sigam funcionando todos os métodos corretamente. (revisar que o arquivo tenha todos os métodos corretamente).
 */
+
+//Importado conforme requisitado
 
 
 
--- programacao-imperativa/checkpoint-III/modulos/aluno.js
+++ programacao-imperativa/checkpoint-III/modulos/aluno.js
@@ -1,76 +1,44 @@
 function alunos(){
 
-    // let alunos_escola = Alunos();
-
-
-    function Aluno(nome,qtd_faltas,notas){
-    this.nome = nome
-    this.qtd_faltas = qtd_faltas
-    this.notas = notas
-    }
-
-    // let aluno1 = new Aluno("ggTestedfdf",2, [1,2,3,4]);
-
-    // for(i=0;i<alunos_escola.length;i++){
-    // //console.log(clientes_banco[i].numero,clientes_banco[i].tipo);
-    // 'let aluno'+[i]+ '= new Aluno('+alunos_escola[i].nome,alunos_escola[i].qtd_faltas, alunos_escola[i].notas+")";
-    // }
-
-    //console.log(aluno2);
-
     let alunos = [
         {
-            "nome":"João cleiton",
-            "qtd_faltas": 2,
+            "nome":"Arthur Campolina",
+            "qtd_faltas": 9,
             "notas": [10,8,6,7]
         },
         {
-            "nome": "Jose da silva",
-            "qtd_faltas": "",
+            "nome":"Cristian Fernandes",
+            "qtd_faltas": 0,
             "notas": [10,8,6,7]
         },
         {
-            "nome": "Maria Jose",
-            "qtd_faltas": 5,
+            "nome":"Felipe Santos",
+            "qtd_faltas": 0,
             "notas": [10,8,6,7]
         },
         {
-            "nome": "Fernando Marinho",
-            "qtd_faltas": 3,
+            "nome":"Kened Silva",
+            "qtd_faltas": 0,
             "notas": [10,8,6,7]
         },
         {
-            "nome": "F",
+            "nome":"Rodrigo Costa Valle",
+            "qtd_faltas": 0,
+            "notas": [10,8,6,7]
+        },
+        {
+            "nome":"Thiago",
             "qtd_faltas": 10,
             "notas": [10,8,6,7]
         },
         {
-            "nome": "F",
-            "qtd_faltas": 7,
+            "nome":"Isabela Wagner",
+            "qtd_faltas": 0,
             "notas": [10,8,6,7]
         },
-        {
-            "nome": "F",
-            "qtd_faltas": 8,
-            "notas": [10,8,6,7]
-        },
-        {
-            "nome": "F",
-            "qtd_faltas": 2,
-            "notas": [10,8,6,7]
-        },
-        {
-            "nome": "F",
-            "qtd_faltas": 4,
-            "notas": [10,8,6,7]
-        },
-        {
-            "nome": "F",
-            "qtd_faltas": 6,
-            "notas": [10,8,6,7]
-        }
       ]
     ;
+
     return alunos
 }
 
